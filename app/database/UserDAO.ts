6600218aae4133c26e0f85d562245fc45191e9e7
Zac Almas <zacalmas@gmail.com>
2023-04-05 16:21:31 -0700

    Logging Update
    API now supports logging that gets stored in Loggly

--- app/database/UserDAO.ts
+++ app/database/UserDAO.ts
@@ -1,12 +1,13 @@
 import { User } from "../models/Users";
 import * as mysql from "mysql";
 import * as util from "util";
+var loggly = require('loggly');
+var logger = loggly.createClient({ token:"e24b11ec-704e-4859-b8b1-a838bd7f5e93", subdomain:"CloudComputingCLC", sendConsoleErrors: false, tag:"ClickerCatEvent-UserDAO" });
 
 /*
 DAO file used for connecting the API to the database
 This DAO handles the users table in our database
 */
-
 export class UserDAO
 {
     private host:string = "clicker-cat.clkzoscxfp7p.us-west-1.rds.amazonaws.com";
@@ -42,13 +43,16 @@ export class UserDAO
      */
     public create(user:User, callback: any)
     {
+        // Logging into loggly
+        logger.log("Entering create() inside UserDAO.ts");
         // Get pooled database connection and run queries   
         this.pool.getConnection(async function(err:any, connection:any)
         {
             connection.release();
 
             // Throw error if an error
-            if (err) throw err;
+            if (err){logger.log(err);}
+            if (err) throw err
 
             // Use Promisfy Util to make an async function and insert User
             connection.query = util.promisify(connection.query);
@@ -56,11 +60,17 @@ export class UserDAO
             let result1 = await connection.query('INSERT INTO `Users` (EMAIL, PASSWORD, NAME) VALUES(?,?,?)', [user.Email, user.Password, user.Name]);
             // If no rows were affected then return -1 to indicate an error
             if(result1.affectedRows != 1)
-               callback(-1);
+            {
+                logger.log("Creating new User failed");
+                callback(-1);
+            }
 
             //getting the id of the newly created User
             let userId = result1.insertId;
 
+            // Logging into loggly
+            logger.log("Creating User Succeeded with an ID of " + userId);
+            logger.log("Exiting create() inside UserDAO.ts");
             // Do a callback to return the results
             callback(userId);
         });
@@ -73,6 +83,7 @@ export class UserDAO
      */
     public findUsers(callback: any)
     {
+        logger.log("Entering findUsers() inside UserDAO.ts");
         // List of Users to return
         let users:User[] = [];
         
@@ -82,11 +93,13 @@ export class UserDAO
             connection.release();
 
             // Throw error if an error
+            if (err){logger.log(err);}
             if (err) throw err;
 
             // Use Promisfy Util to make an async function and run query to get all users
             connection.query = util.promisify(connection.query);
             // Database query assigned to a result variable
+            logger.log("Collecting users from the database...");
             let result1 = await connection.query('SELECT * FROM `Users`');
             // Looping over the results and adding each user to the list
             for(let x=0;x < result1.length;++x)
@@ -94,7 +107,8 @@ export class UserDAO
                 // Add user and its data to the list
                 users.push(new User(result1[x].ID, result1[x].EMAIL, result1[x].PASSWORD, result1[x].NAME, result1[x].ROLE));
             }
-
+            logger.log("Users collected.");
+            logger.log("Exiting findUsers() inside UserDAO.ts");
             // Do a callback to return the results
             callback(users);
          });
@@ -108,6 +122,8 @@ export class UserDAO
      */
     public findUserById(id:number, callback: any)
     {
+        // Logging into loggly
+        logger.log("Entering findUserById() inside UserDAO.ts");
         // User that's going to be returned
         let user:User;
 
@@ -117,11 +133,13 @@ export class UserDAO
             connection.release();
 
             // Throw error if an error
+            if (err){logger.log(err);}
             if (err) throw err;
 
             // Use Promisfy Util to make an async function and run query to get all Users for search
             connection.query = util.promisify(connection.query);
             // Database query assigned to a result variable
+            logger.log("Collecting user from the database...");
             let result1 = await connection.query("SELECT * FROM `Users` WHERE ID = ?", id);
             // Assigning the result to the user model using a loop
             for(let x=0;x < result1.length;++x)
@@ -129,6 +147,9 @@ export class UserDAO
                 // Get user from the database to return
                 user = new User(result1[x].ID, result1[x].EMAIL, result1[x].PASSWORD, result1[x].NAME, result1[x].ROLE);
             }
+            logger.log("User collected");
+            // Logging into loggly
+            logger.log("Exiting findUserById() inside UserDAO.ts");
             // Do a callback to return the results
             callback(user);
         });
@@ -142,6 +163,8 @@ export class UserDAO
      */
     public findUserByEmail(email:string, callback: any)
     {
+        // Logging into loggly
+        logger.log("Entering findUserByEmail() inside UserDAO.ts");
         // User that's going to be returned
         let user:User;
 
@@ -151,11 +174,13 @@ export class UserDAO
             connection.release();
 
             // Throw error if an error
+            if (err){logger.log(err);}
             if (err) throw err;
 
             // Use Promisfy Util to make an async function and run query to get all Users for search
             connection.query = util.promisify(connection.query);
             // Database query assigned to a result variable
+            logger.log("Collecting user from the database...");
             let result1 = await connection.query("SELECT * FROM `Users` WHERE EMAIL = ?", email);
             // Adding the result to the user model 
             for(let x=0;x < result1.length;++x)
@@ -163,6 +188,9 @@ export class UserDAO
                 // Get user from the database to return
                 user = new User(result1[x].ID, result1[x].EMAIL, result1[x].PASSWORD, result1[x].NAME, result1[x].ROLE);
             }
+            logger.log("User collected");
+            // Logging into loggly
+            logger.log("Exiting findUserByEmail() inside UserDAO.ts");
             // Do a callback to return the results
             callback(user);
         });
@@ -176,29 +204,36 @@ export class UserDAO
      */
     public update(user:User, callback: any)
     {
-         // Get pooled database connection and run queries   
-         this.pool.getConnection(async function(err:any, connection:any)
-         {
+        // Logging into loggly
+        logger.log("Entering update() inside UserDAO.ts");
+        // Get pooled database connection and run queries   
+        this.pool.getConnection(async function(err:any, connection:any)
+        {
             connection.release();
 
-             // Throw error if an error
+            // Throw error if an error
+            if (err){logger.log(err);}
             if (err) throw err;
  
-             // Use Promisfy Util to make an async function and update User
+            // Use Promisfy Util to make an async function and update User
             let changes = 0;
             // Use Promisfy Util to make an async function and insert User
             connection.query = util.promisify(connection.query);
             // Database query assigned to a result variable
+            logger.log("Attempting to update user...");
             let result1 = await connection.query("UPDATE `Users` SET EMAIL=?, PASSWORD=?, NAME=?", [user.Email, user.Password, user.Name]);
             // If any row was affected in the database, update the changes variable to reflect that
             if(result1.changedRows != 0)
                 ++changes;
             // Log Changes
             console.log(changes);
+            logger.log("Changes made: " + changes);
+            // Logging into loggly
+            logger.log("Exiting update() inside UserDAO.ts");
             // Do a callback to return the results
             callback(changes);
-         });
-     }
+        });
+    }
 
      /**
      * CRUD method to delete a User.
@@ -208,23 +243,30 @@ export class UserDAO
      * */
     public delete(userId:number, callback: any)
     {
+        // Logging into loggly
+        logger.log("Entering delete() inside UserDAO.ts");
         // Get pooled database connection and run queries   
         this.pool.getConnection(async function(err:any, connection:any)
         {
             connection.release();
             
             // Throw error if an error
-           if (err) throw err;
+            if (err){logger.log(err);}
+            if (err) throw err;
 
             // Use Promisfy Util to make an async function and run query to delete User
             let changes = 0;
             // Use Promisfy Util to make an async function and insert User
             connection.query = util.promisify(connection.query);
             // Database query assigned to a result variable
+            logger.log("Attempting to delete user from the database...");
             let result1 = await connection.query('DELETE FROM `Users` WHERE ID=?', [userId]);
             // Set the affected rows to the changes variable
             changes = changes + result1.affectedRows;
 
+            // Logging into loggly
+            logger.log("User's deleted: " + changes);
+            logger.log("Exiting delete() inside UserDAO.ts");
             // Do a callback to return the results
             callback(changes);
         });
--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const app = express();
 const port = 3000;
 app.use(cors());
 app.use(bodyParser.json());
+var loggly = require('loggly');
+var logger = loggly.createClient({ token:"e24b11ec-704e-4859-b8b1-a838bd7f5e93", subdomain:"CloudComputingCLC", sendConsoleErrors: false, tag:"ClickerCatEvent-Index" });
 
 // Database configuration
 const dbHost = "clicker-cat.clkzoscxfp7p.us-west-1.rds.amazonaws.com"
@@ -29,15 +31,23 @@ app.use(express.static('app/images'))
  */
 app.get('/', function (_req, res)
 {
+    // Sending a log statement to loggly
+    logger.log("Entering the default ('/') route");
     // Log the location
     console.log('In GET / Route');
+    // Sending a log statement to loggly
+    logger.log("Exiting the default ('/') route");
     // Return Test Text
     res.send('This is the default root Route.');
 })
 
 app.get('/testroute', function (_req, res)
 {
+    // Sending a log statement to loggly
+    logger.log("Entering test route");
     console.log('Okay this works');
+    // Sending a log statement to loggly
+    logger.log("Exiting test route");
     res.send('The testing route works');
 })
 
@@ -48,6 +58,8 @@ app.get('/testroute', function (_req, res)
  */
 app.get('/users', function (_req, res)
 {
+    // Sending a log statement to loggly
+    logger.log("Entering the get all users ('/users') route");
     // Return User List as JSON, call SicknassDAO.findUsers(), and return JSON array of Users (a string)
     // Log the location
     console.log('In GET /users Route');
@@ -56,6 +68,8 @@ app.get('/users', function (_req, res)
     // Using the findUsers function, return all of the users
     dao.findUsers(function(user)
     {
+        // Sending a log statement to loggly
+        logger.log("Exiting the get all users ('/users') route");
         res.json(user); // Responding with a JSON of all users
     });
 })
@@ -67,6 +81,8 @@ app.get('/users', function (_req, res)
  */
 app.get('/users/search/user/:id', function (req, res)
 {
+    // Sending a log statement to loggly
+    logger.log("Entering the get user by id ('/users/search/user/:id') route");
     // Return users List as JSON, call UserDAO.findUserById(), and return JSON array of Users
     // Log the location and the request parameters
     console.log('In GET /users/searach/user Route for ' + req.params.id);
@@ -77,8 +93,14 @@ app.get('/users/search/user/:id', function (req, res)
     {
         if (user == null)
         {
+            // Sending a log statement to loggly
+            logger.log("INVALID USER ID");
+            logger.log("Exiting the get user by id ('/users/search/user/:id') route");
             res.status(200).json({error: "INVALID USER ID"}); // If no user was returned by the DAO then the ID was invalid
         } else {
+            // Sending a log statement to loggly
+            logger.log("USER FOUND");
+            logger.log("Exiting the get user by id ('/users/search/user/:id') route");
             res.status(200).json(user); // If a user was returned by the DAO, put the user in the response
         }
     });
@@ -91,6 +113,8 @@ app.get('/users/search/user/:id', function (req, res)
  */
 app.get('/users/search/user/email/:email', function (req, res)
 {
+    // Sending a log statement to loggly
+    logger.log("Entering the get user by email ('/users/search/user/email/:email') route");
     // Return users List as JSON, call UserDAO.findUserById(), and return JSON array of Users
     // Log the location and the request parameters 
     console.log('In GET /users/searach/user/email Route for ' + req.params.email);
@@ -101,8 +125,14 @@ app.get('/users/search/user/email/:email', function (req, res)
     {
         if (user == null)
         {
+            // Sending a log statement to loggly
+            logger.log("USER NOT FOUND");
+            logger.log("Exiting the get user by email ('/users/search/user/email/:email') route");
             res.status(200).json({error: "USER NOT FOUND"}); //If the user was not returned by the DAO, then the user was not found
         } else {
+            // Sending a log statement to loggly
+            logger.log("USER FOUND");
+            logger.log("Exiting the get user by email ('/users/search/user/email/:email') route");
             res.status(200).json(user); //If the user was returned by the DAO, put the user into the response
         }
     });
@@ -115,6 +145,8 @@ app.get('/users/search/user/email/:email', function (req, res)
  */
 app.post('/users', function (req, res)
 {
+    // Sending a log statement to loggly
+    logger.log("Entering the post new user ('/users') route");
     //Adding CryptoJS for encrypting the passwords
     const CryptoJS = require('crypto-js');
     
@@ -122,6 +154,9 @@ app.post('/users', function (req, res)
     console.log('In POST /users Route with Post of ' + JSON.stringify(req.body));
     if(!req.body)
     {
+        // Sending a log statement to loggly
+        logger.log("INVALID USER POSTED");
+        logger.log("Exiting the post new user ('/users') route");
         // Check for valid POST Body, note this should validate EVERY field of the POST
         res.status(400).json({error: "Invalid User Posted"});
     }
@@ -137,10 +172,16 @@ app.post('/users', function (req, res)
         let dao = new UserDAO(dbHost, dbPort, dbUsername, dbPassword);
         dao.create(user, function(userId)
         {
-            if(userId == -1)
+            if(userId == -1){
+                // Sending a log statement to loggly
+                logger.log("Exiting the post new user ('/users') route");
                 res.status(200).json({"error" : "Creating User failed"}) //If the response is -1 then something went wrong and the user was not created
-            else
+            }
+            else{
+                // Sending a log statement to loggly
+                logger.log("Exiting the post new user ('/users') route");
                 res.status(200).json({"success" : "Creating User passed with an ID of " + userId}); //If the response was anything other than -1 the user was created
+            }
         });     
       }
 })
@@ -152,6 +193,8 @@ app.post('/users', function (req, res)
  */
 app.post('/users/login', function (req, res)
 {
+    // Sending a log statement to loggly
+    logger.log("Entering the post login user ('/users/login') route");
     //Adding CryptoJS for encrypting the passwords
     const CryptoJS = require('crypto-js');
 
@@ -168,13 +211,21 @@ app.post('/users/login', function (req, res)
     dao.findUserByEmail(req.body.email, function(user){
         if (user == null)
         {
+            // Sending a log statement to loggly
+            logger.log("USER NOT FOUND");
+            logger.log("Exiting the post login user ('/users/login') route");
             res.status(201).json({error: "USER NOT FOUND"}); //If the user was not returned by the DAO, then the user was not found
         } else {
             if (user.password == hash) //Testing if the password matches the password of the user found in the database
             {
-                console.log("USER FOUND");
+                // Sending a log statement to loggly
+                logger.log("USER FOUND");
+                logger.log("Exiting the post login user ('/users/login') route");
                 res.status(200).json(user); //If the user was returned by the DAO and the password was correct, put the user into the response
             } else {
+                // Sending a log statement to loggly
+                logger.log("INCORRECT PASSWORD");
+                logger.log("Exiting the post login user ('/users/login') route");
                 res.status(202).json({error: "INCORRECT PASSWORD"}); // If this error is thrown then the user was found but the password was incorrect
             }
         }
@@ -188,6 +239,8 @@ app.post('/users/login', function (req, res)
  */
 app.delete('/users/:id', function (req, res)
 {
+    // Sending a log statement to loggly
+    logger.log("Entering the delete user by id ('/users/:id') route");
     // Get the user
     console.log('In DELETE /users Route with ID of ' + req.params.id);
     let userId = Number(req.params.id);
@@ -196,10 +249,18 @@ app.delete('/users/:id', function (req, res)
     let dao = new UserDAO(dbHost, dbPort, dbUsername, dbPassword);
     dao.delete(userId, function(changes)
     {
-        if(changes == 0)
+        if(changes == 0){
+            // Sending a log statement to loggly
+            logger.log("Delete User: FAILED");
+            logger.log("Exiting the delete user by id ('/users/:id') route");
             res.status(200).json({"error" : "Delete User failed"}) //If zero changes were made to the database then the user was not deleted
-        else
+        }
+        else{
+            // Sending a log statement to loggly
+            logger.log("Delete User: SUCCESS");
+            logger.log("Exiting the delete user by id ('/users/:id') route");
             res.status(200).json({"success" : "Delete User passed"}) //If changes were made to the database then the user was deleted
+        }
     });
  })
 
@@ -210,12 +271,17 @@ app.delete('/users/:id', function (req, res)
  */
 app.put('/users', function (req, res)
 {
+    // Sending a log statement to loggly
+    logger.log("Entering the update user ('/users') route");
     //Logging the request body
     console.log(req.body);
     // If invalid PUT Body then return 400 response else update user in the database
     console.log('In PUT /users Route with Post of ' + JSON.stringify(req.body));
     if(!req.body)
     {
+        // Sending a log statement to loggly
+        logger.log("INVALID USER POSTED");
+        logger.log("Exiting the update user ('/users/:id') route");
         // Check for valid PUT Body, note this should validate EVERY field of the POST
         res.status(400).json({error: "Invalid user Posted"});
     }
@@ -229,9 +295,18 @@ app.put('/users', function (req, res)
         dao.update(user, function(changes)
         {
             if(changes == 0)
+            {
+                // Sending a log statement to loggly
+                logger.log("Updating User: SUCCESS (However nothing was changed...)");
+                logger.log("Exiting the update user ('/users') route");
                 res.status(200).json({error : "Updating User passed but nothing was changed"}) //If there are no changes made to the database, then it will return this status
-            else
+            }
+            else{
+                // Sending a log statement to loggly
+                logger.log("Updating User: SUCCESS");
+                logger.log("Exiting the update user ('/users') route");
                 res.status(200).json({success : "Updating User passed and data was changed"}); //If there are changes made to the database, then it will return this status
+            }
         });     
       }
 })
@@ -244,4 +319,5 @@ app.put('/users', function (req, res)
 app.listen(port, () => 
 {
     console.log(`Example app listening on port ${port}!`);
+    logger.log("Clickercat is running");
 });
